614e2e3c39135cf4bb3e6fccd002b3af664d8221
Noam Cohen <noam.cohen@zooz.com>
2019-04-02 17:09:57 +0300

    Override KafkaMessage.toString method

--- app/com/zooz/kafkas3connector/kafka/KafkaMessage.scala
+++ app/com/zooz/kafkas3connector/kafka/KafkaMessage.scala
@@ -5,6 +5,7 @@ import org.joda.time.format.DateTimeFormat
 import play.api.libs.json.Json
 import play.api.libs.json.JsObject
 import java.nio.ByteBuffer
+import com.zooz.kafkas3connector.kafka.bytebufferconverters.StringConverters._
 
 /** Case class representing a Kafka message */
 case class KafkaMessage(
@@ -20,5 +21,12 @@ case class KafkaMessage(
    */
   def creationDateTime: DateTime = {
     new DateTime(creationEpoch)
+  }
+
+  override def toString: String = {
+    val keyAsString: String = Option(key).map(_.asString).getOrElse("")
+    val valueAsString: String = Option(value).map(_.asString).getOrElse("")
+    
+    s"KafkaMessage($topic, $partition, $offset, $creationEpoch, ByteBuffer(${keyAsString}), ByteBuffer(${valueAsString}))"  // scalastyle:off
   }
 }
